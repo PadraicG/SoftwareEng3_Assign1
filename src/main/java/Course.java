bb14df596688d84103c5658ca850cedd4776916c
PadraicG <gaffneypadraic@gmail.com>
2022-10-17 22:24:54 +0100

    more parameter updates

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -7,4 +7,12 @@ public class Course {
     private DateTime endDate = new DateTime();
      private DateTime startDate = new DateTime();
 
+     public Course(String name, DateTime startDate, DateTime endDate)
+     {
+    this.name = name;
+    this.startDate = startDate;
+    this.endDate = endDate;
+
+     }
+
 }
--- src/main/java/Module.java
+++ src/main/java/Module.java
@@ -4,4 +4,10 @@ public class Module {
     private Student[] students;
     private Course[] associatedCourses;
     private Lecturer associatedLecturer;
+
+    public Module(String name, String id){
+
+        this.name = name;
+        this.id = id;
+    }
 }
