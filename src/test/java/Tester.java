0c201a418ca3e923145ef68fca0d6fa625abb9f8
PadraicG <gaffneypadraic@gmail.com>
2022-10-23 12:19:48 +0100

    Latest Changes

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,22 +1,27 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Tester {
 
     public static void main(String[] args){
-        Student student = new Student("Podge Gaffney", 5, 18455202 , "14/05/2000");
-        Lecturer lect = new Lecturer("Alan Watts", 8, 18455202, "20/12/1940");
+        Student student = new Student("Podge Gaffney", 22, 18455202 , "14/05/2000");
+        Lecturer lect = new Lecturer("Alan Watts", 62, 18455202, "20/12/1960");
         student.getUserName();
         System.out.println(student.getUserName());
         System.out.println(lect.getUserName());
 
 
-    }
+
 
     @Test
-    void testMethods(){
-        //assertTrue(student.userName)
+            String userName = student.getUserName();
+            String obj2 = "Podge Gaffney18455202";
+    public void testMethods()
+    {
+        assertEquals("Podge Gaffney18455202", userName);
     }
 
+}
 }
